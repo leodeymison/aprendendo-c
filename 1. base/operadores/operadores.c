3e2645e395259643409862cffd22adb03c89b44a
leodeymison <leodeymisonbasquete@gmail.com>
2023-01-29 00:09:09 -0300

    operations

---
+++ 1. base/operadores/operadores.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    system("clear");
+    int valueOne = 23;
+    int valueTwo = 10;
+    printf("Soma: %i\n", valueOne + valueTwo);          // Soma
+    printf("Subtração: %i\n", valueOne - valueTwo);     // Subtração
+    printf("Multiplicação: %i\n", valueOne * valueTwo); // Multiplicação
+    printf("Divisão: %i\n", valueOne / valueTwo);       // Divisão
+
+    printf("\n\n");
+    return;
+    system("pause");
+}
